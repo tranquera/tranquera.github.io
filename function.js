8889df4fb96a94cb93ae4efbb795930ad5601fb9
tranquera <126631384+tranquera@users.noreply.github.com>
2023-03-06 22:24:33 -0300

    Add files via upload
    initial commit

---
+++ function.js
@@ -0,0 +1,10 @@
+function todo_listo(){
+document.getElementById("boton1").onclick=iniciar;
+document.getElementById("boton2").onclick=ir_a_mi_sitio_web;
+}
+
+ function ir_a_mi_sitio_web(){
+ window.location.href='https://www.portaldearquitectos.com';
+ }
+
+
